ed301cf51748d1c046ab75f86fc67de764485e4e
Brett Jurman <brett@thedeveffect.com>
2022-04-02 16:34:29 -0400

    insanity

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { Kai } from "@kaimerra-corp/kai-api/dist/index";
 import { Engine, Render, Runner, Bodies, Composite } from "matter-js";
 
+import "../assets/css.css";
+import * as aether from "../assets/floweraethersingle.png";
+
 (async () => {
   const kai = await Kai.createForBrowser();
 
@@ -11,16 +14,54 @@ import { Engine, Render, Runner, Bodies, Composite } from "matter-js";
   const render = Render.create({
     element: document.body,
     engine: engine,
+    options: {
+      wireframes: false,
+      background: "transparent",
+    },
   });
 
+  // create plinkers
+  const plinkers = [];
+  for (let i = 0; i < 50; ++i) {
+    plinkers.push(
+      Bodies.circle(Math.random() * 800, Math.random() * 400, 5, {
+        isStatic: true,
+        restitution: 1.0,
+        render: {
+          fillStyle: "white",
+        },
+      })
+    );
+  }
+
   // create two boxes and a ground
-  const boxA = Bodies.circle(400, 50, 20);
-  const boxB = Bodies.rectangle(400, 200, 80, 80);
-  const ground = Bodies.rectangle(400, 600, 810, 60, { isStatic: true });
-  const cieling = Bodies.rectangle(400, 10, 810, 60, { isStatic: true });
+  const balls = [];
+  for (let i = 0; i < 1000; ++i) {
+    balls.push(
+      Bodies.circle(Math.random() * 400, 50 - Math.random()*2000, 12, {
+        render: {
+          sprite: {
+            texture: aether,
+            xScale: 2.0,
+            yScale: 2.0,
+          },
+        },
+        restitution: 1.0,
+      })
+    );
+  }
+
+  const boxB = Bodies.rectangle(400, 200, 80, 80, {
+    restitution: 1.0,
+  });
+  const ground = Bodies.rectangle(400, 600, 810, 60, {
+    isStatic: true,
+    restitution: 1.0,
+  });
+  //const cieling = Bodies.rectangle(400, 10, 810, 60, { isStatic: true });
 
   // add all of the bodies to the world
-  Composite.add(engine.world, [boxA, boxB, ground, cieling]);
+  Composite.add(engine.world, [ground, boxB, ...balls, ...plinkers]);
 
   // run the renderer
   Render.run(render);
@@ -28,11 +69,12 @@ import { Engine, Render, Runner, Bodies, Composite } from "matter-js";
   // create runner
   const runner = Runner.create();
 
+  /*
   kai.on("any", (counters) => {
     const air = counters.get("air");
     const earth = counters.get("earth");
     engine.gravity.y = (earth - air) / air;
-  });
+  });*/
 
   // run the engine
   Runner.run(runner, engine);
