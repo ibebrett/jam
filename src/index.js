f9a9085c88ea8e51e08303b40d4b74303c2aa2d6
Brett Jurman <brett@thedeveffect.com>
2022-04-03 10:39:58 -0400

    removing some features, setting it up to run without kaipod

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import { Kai } from "@kaimerra-corp/kai-api/dist/index";
-import { Engine, Render, Runner, Bodies, Composite, Events } from "matter-js";
+import {
+  Mouse,
+  MouseConstraint,
+  Engine,
+  Render,
+  Runner,
+  Bodies,
+  Composite,
+  Events,
+} from "matter-js";
 
 import "../assets/css.css";
 import * as aether from "../assets/floweraethersingle.png";
@@ -17,17 +26,22 @@ const everyN = (n, f) => {
 };
 
 (async () => {
-  const kai = await Kai.createForBrowser();
+  let kai;
+  try { 
+    kai = await Kai.createForBrowser();
+  } catch (error) {
+    console.log("Could not connect to kaipod!");
+  }
 
   // create an engine
   const engine = Engine.create();
 
   const balls = [];
 
-  const update = everyN(10, () => {
+  const update = everyN(4, () => {
     const ball = Bodies.circle(
-      Math.random() * 400,
-      50 - Math.random() * 2000,
+      Math.random() * 800,
+      200 - Math.random() * 1000,
       12,
       {
         render: {
@@ -61,18 +75,34 @@ const everyN = (n, f) => {
     },
   });
 
+  const mouse = Mouse.create(render.canvas);
+
+  const mouseConstraint = MouseConstraint.create(engine, { mouse });
+
+  /*
+  Events.on(mouseConstraint, "mousedown", function (event) {
+    console.log(event);
+    engine.gravity.x = (event.mouse.absolute.x - 400) / 200.0;
+    engine.gravity.y = (event.mouse.absolute.y - 400) / 200.0;
+  });*/
+
   // create plinkers
   const plinkers = [];
-  for (let i = 0; i < 100; ++i) {
-    plinkers.push(
-      Bodies.circle(Math.random() * 800, Math.random() * 400, 5, {
-        isStatic: true,
-        restitution: 0.5,
-        render: {
-          fillStyle: "white",
-        },
-      })
-    );
+
+  let flip = false;
+  for (let y = 0; y < 600; y += 60) {
+    for (let x = 0; x < 800; x += 60) {
+      plinkers.push(
+        Bodies.circle(x + (flip ? 30 : 0), y, 5, {
+          isStatic: true,
+          restitution: 0.5,
+          render: {
+            fillStyle: "white",
+          },
+        })
+      );
+    }
+    flip = !flip;
   }
 
   // create two boxes and a ground
@@ -93,9 +123,6 @@ const everyN = (n, f) => {
     );
   }*/
 
-  const boxB = Bodies.rectangle(400, 200, 80, 80, {
-    restitution: 1.0,
-  });
   const ground = Bodies.rectangle(400, 600, 810, 60, {
     isStatic: true,
     restitution: 1.0,
@@ -103,7 +130,7 @@ const everyN = (n, f) => {
   //const cieling = Bodies.rectangle(400, 10, 810, 60, { isStatic: true });
 
   // add all of the bodies to the world
-  Composite.add(engine.world, [ground, boxB, ...plinkers]);
+  Composite.add(engine.world, [ground, ...plinkers]);
 
   // run the renderer
   Render.run(render);
@@ -111,12 +138,12 @@ const everyN = (n, f) => {
   // create runner
   const runner = Runner.create();
 
-  /*
-  kai.on("any", (counters) => {
+  //engine.gravity.y = -1;
+  kai && kai.on("any", (counters) => {
     const air = counters.get("air");
     const earth = counters.get("earth");
     engine.gravity.y = (earth - air) / air;
-  });*/
+  });
 
   // run the engine
   Runner.run(runner, engine);
